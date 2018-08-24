265e9eefcffb7d06e53e160898a0454a80cf1649
Aurélie Merlo <aurelie.merlo@lifen.fr>
2018-08-24 02:23:44 +0200

    fix typo

--- config/prod.exs
+++ config/prod.exs
@@ -21,7 +21,7 @@ config :elixir_pdg, ElixirPdgWeb.Endpoint,
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
-config :elixir_pdg, ElixirPdgWeb.Repo,
+config :elixir_pdg, ElixirPdg.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
